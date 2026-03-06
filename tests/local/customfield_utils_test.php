ca44aa0ad7fe10bf0cd95e9ff405240902b412f6
Laurent David <lmedavid@gmail.com>
2026-03-06 08:11:49 +0100

    Add local namespace unit tests for utils classes

---
+++ tests/local/customfield_utils_test.php
@@ -0,0 +1,104 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for local customfield utils.
+ *
+ * @package    local_activitylibrary
+ * @copyright  2026 CALL Learning
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_activitylibrary\local;
+
+use local_activitylibrary\customfield\coursemodule_handler;
+use local_activitylibrary\local\filters\text_filter;
+use local_activitylibrary\test\testcase;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Unit tests for customfield_utils.
+ */
+final class customfield_utils_test extends testcase {
+    /**
+     * Return a coursemodule custom field by shortname.
+     *
+     * @param string $shortname
+     * @return \core_customfield\field_controller
+     */
+    private function get_coursemodule_field_by_shortname(string $shortname): \core_customfield\field_controller {
+        $handler = coursemodule_handler::create();
+        foreach ($handler->get_fields() as $field) {
+            if ($field->get('shortname') === $shortname) {
+                return $field;
+            }
+        }
+        $this->fail('Expected customfield not found: ' . $shortname);
+    }
+
+    /**
+     * Test field name normalization helper.
+     *
+     * @covers \local_activitylibrary\local\customfield_utils::get_field_name
+     */
+    public function test_get_field_name_normalizes_shortname(): void {
+        $fieldname = customfield_utils::get_field_name('customfield', '  F1  ');
+        $this->assertEquals('customfield_f1', $fieldname);
+    }
+
+    /**
+     * Test data column lookup for a custom field.
+     *
+     * @covers \local_activitylibrary\local\customfield_utils::get_datafieldcolumn_value_from_field_handler
+     */
+    public function test_get_datafieldcolumn_value_from_field_handler(): void {
+        $field = $this->get_coursemodule_field_by_shortname('f1');
+        $column = customfield_utils::get_datafieldcolumn_value_from_field_handler($field);
+        $this->assertNotEmpty($column);
+        $this->assertIsString($column);
+    }
+
+    /**
+     * Test filter factory returns the expected filter instance.
+     *
+     * @covers \local_activitylibrary\local\customfield_utils::get_filter_from_field
+     */
+    public function test_get_filter_from_field_returns_matching_filter(): void {
+        $field = $this->get_coursemodule_field_by_shortname('f1');
+        $filter = customfield_utils::get_filter_from_field($field);
+
+        $this->assertInstanceOf(text_filter::class, $filter);
+    }
+
+    /**
+     * Test SQL fragment generation from customfield filters.
+     *
+     * @covers \local_activitylibrary\local\customfield_utils::get_sql_from_filters_handler
+     */
+    public function test_get_sql_from_filters_handler_builds_where_and_params(): void {
+        $handler = coursemodule_handler::create();
+        [$sqlwhere, $sqlparams] = customfield_utils::get_sql_from_filters_handler(
+            [['shortname' => 'f1', 'value' => 'needle']],
+            $handler
+        );
+
+        $this->assertStringContainsString('LIKE', $sqlwhere);
+        $this->assertNotEmpty($sqlparams);
+        $this->assertContains('%needle%', array_values($sqlparams));
+    }
+}
+
---
+++ tests/local/utils_test.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for local utils.
+ *
+ * @package    local_activitylibrary
+ * @copyright  2026 CALL Learning
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_activitylibrary\local;
+
+use local_activitylibrary\customfield\coursemodule_handler;
+use local_activitylibrary\test\testcase;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Unit tests for utils.
+ */
+final class utils_test extends testcase {
+    /**
+     * Reset hidden fields static cache.
+     */
+    private function reset_hiddenfields_cache(): void {
+        $property = new \ReflectionProperty(utils::class, 'hiddenfields');
+        $property->setAccessible(true);
+        $property->setValue(null, null);
+    }
+
+    /**
+     * Test handler component helpers.
+     *
+     * @covers \local_activitylibrary\local\utils::get_handler_full_component
+     * @covers \local_activitylibrary\local\utils::get_hidden_filter_config_name
+     */
+    public function test_handler_component_helpers(): void {
+        $handler = coursemodule_handler::create();
+
+        $this->assertEquals('local_activitylibrary_coursemodule', utils::get_handler_full_component($handler));
+        $this->assertEquals(
+            'filter_hidden_local_activitylibrary_coursemodule',
+            utils::get_hidden_filter_config_name($handler)
+        );
+    }
+
+    /**
+     * Test hidden fields retrieval returns empty when no config exists.
+     *
+     * @covers \local_activitylibrary\local\utils::get_hidden_fields_filters
+     */
+    public function test_get_hidden_fields_filters_returns_empty_without_config(): void {
+        $handler = coursemodule_handler::create();
+        $this->reset_hiddenfields_cache();
+
+        $hidden = utils::get_hidden_fields_filters($handler);
+        $this->assertSame([], $hidden);
+    }
+
+    /**
+     * Test catalog URL helper returns expected local URL.
+     *
+     * @covers \local_activitylibrary\local\utils::get_catalog_url
+     */
+    public function test_get_catalog_url_returns_local_activitylibrary_url(): void {
+        [$text, $url] = utils::get_catalog_url();
+
+        $this->assertNotEmpty($text);
+        $this->assertInstanceOf(\moodle_url::class, $url);
+        $this->assertStringContainsString('/local/activitylibrary/index.php', $url->out(false));
+    }
+}
+
